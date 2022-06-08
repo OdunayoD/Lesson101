1df245c39ea810287b5966db34c356b7f50c207e
OdunayoD <odunayoadegbaju@yahoo.com>
2022-06-07 20:07:50 -0700

    Working more on JS

---
+++ web/Javascript/conversion.js
@@ -0,0 +1,44 @@
+/**
+ * Prompt a user for a first number and save it in a variable.
+ * Prompt a user for a second number and save it in another variable.
+ * Multiply the two numbers and log the result.
+*/ 
+
+let first = prompt("first number");
+
+first = Number(first);
+
+let second = prompt("second number");
+
+second = Number(second);
+
+let result = first % second;
+
+console.log(result);
+
+
+
+
+
+
+
+let a = 3 + 6; // This is an expression with an assignment and an operator.
+
+let mod = 6 % 2; // 0.
+
+let mod2 = 4 % 2 // 0
+
+let mod3 = 28 % 2 // 0
+
+let mod4 = 7 % 2 // 1
+
+let mod5 = 19 % 2 // 1
+
+let mod6 = 47 % 2 // 1
+
+let mod7 = 54 % 2 // 0
+
+let mod8 = 69 % 2 // 1
+
+
+
